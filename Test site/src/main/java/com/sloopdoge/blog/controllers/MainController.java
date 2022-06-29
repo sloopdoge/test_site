5badf023a67d48af5a518f619c51009c78c93794
sloopdoge <vad.slupko.ukr@gmail.com>
2022-06-30 01:14:31 +0300

    Added pages and cross walks through them. Next stage is MySQL

---
+++ Test site/src/main/java/com/sloopdoge/blog/controllers/MainController.java
@@ -0,0 +1,34 @@
+package com.sloopdoge.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    /* Controller for HOME PAGE*/
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Home");
+        return "home";
+    }
+    /* Controller for Dashboard PAGE*/
+    @GetMapping("/dashboard")
+    public String dashboard(Model model) {
+        model.addAttribute("title", "Dashboard");
+        return "dashboard";
+    }
+    /* Controller for Products PAGE*/
+    @GetMapping("/products")
+    public String products(Model model) {
+        model.addAttribute("title", "Products");
+        return "products";
+    }
+    /* Controller for About PAGE*/
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "About us");
+        return "about";
+    }
+
+}
