c539c9aa6289cbf600678a4e6cc4d142397f3379
Vlad Slupko <48050813+sloopdoge@users.noreply.github.com>
2022-06-29 23:27:57 +0300

    Created start files for Spring Boot Project

---
+++ Test site/src/main/java/com/sloopdoge/blog/TestSiteApplication.java
@@ -0,0 +1,13 @@
+package com.sloopdoge.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSiteApplication.class, args);
+    }
+
+}
