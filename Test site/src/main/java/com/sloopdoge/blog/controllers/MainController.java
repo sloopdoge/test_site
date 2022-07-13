6caeddd583aa24472cf3f6bfdb75c6df956c11be
sloopdoge <vad.slupko.ukr@gmail.com>
2022-07-13 15:37:16 +0300

    Created connection with MySql table to show item in the products page(name, price)

--- Test site/src/main/java/com/sloopdoge/blog/controllers/MainController.java
+++ Test site/src/main/java/com/sloopdoge/blog/controllers/MainController.java
@@ -1,46 +1,60 @@
 package com.sloopdoge.blog.controllers;
 
+import com.sloopdoge.blog.models.Items;
+import com.sloopdoge.blog.repo.ItemsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private ItemsRepository itemsRepository;
+
     /* Controller for HOME PAGE*/
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("title", "Home");
         return "home";
     }
+
     /* Controller for Dashboard PAGE*/
     @GetMapping("/dashboard")
     public String dashboard(Model model) {
         model.addAttribute("title", "Dashboard");
         return "dashboard";
     }
+
     /* Controller for Products PAGE*/
     @GetMapping("/products")
     public String products(Model model) {
-        model.addAttribute("title", "Products");
+        Iterable<Items> items = itemsRepository.findAll();
+        model.addAttribute("items", items);
         return "products";
     }
+
     /* Controller for About PAGE*/
     @GetMapping("/about")
     public String about(Model model) {
         model.addAttribute("title", "About us");
         return "about";
     }
+
     /* Controller for Login PAGE*/
     @GetMapping("/login")
     public String login(Model model) {
         model.addAttribute("title", "Login page");
         return "login";
     }
+
     /* Controller for Sign up PAGE*/
     @GetMapping("/sign_up")
     public String sign_up(Model model) {
         model.addAttribute("title", "Sign up page");
         return "sign_up";
     }
+
 
 }
